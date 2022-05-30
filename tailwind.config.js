e2a1ce5834e7f51c3557b9eec67346c7d33841cb
Sinh Fu San <ss12932@my.bristol.ac.uk>
2022-05-30 19:58:02 +0100

    finish nav component

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,15 @@ module.exports = {
       "2xl": "1536px",
     },
     extend: {
+      keyframes: {
+        radiate: {
+          "0%": { boxShadow: "0 0 0 0 rgba(255,255,255,1)" },
+          "70%": { boxShadow: "0 0 2rem 1.5rem rgba(255,255,255, 0)" },
+        },
+      },
+      animation: {
+        radiate: "radiate 2s infinite",
+      },
       colors: {
         "rich-black-fogra-29": "#001219ff",
         "blue-sapphire": " #005f73ff",
@@ -19,6 +28,8 @@ module.exports = {
         "alloy-orange": "#ca6702ff",
         rust: "#bb3e03ff",
         rufous: "#ae2012ff",
+        "sonic-silver": "#6c757dff",
+        cultured: "#f8f9faff",
         "ruby-red": "#9b2226ff",
       },
     },
