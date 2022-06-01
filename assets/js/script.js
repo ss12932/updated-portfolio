c546b6edaabd3cfb972993cec901bb55f626f427
Sinh Fu San <ss12932@my.bristol.ac.uk>
2022-06-01 12:04:18 +0100

    finish about me section

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,3 +47,42 @@ document.querySelectorAll(".nav-link ").forEach((link) =>
     navBar.classList.remove("active");
   })
 );
+
+const handleHoverImages = (e) => {
+  const target = e.target;
+  if (target.matches("a")) {
+    const href = target.getAttribute("href");
+    if (href === "#section-1") {
+      section1.style.backgroundImage =
+        "linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('/assets/img/tom-w-zwdkxQZu0Ko-unsplash.jpg')";
+    }
+    if (href === "#section-2") {
+      section1.style.backgroundImage =
+        "linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('/assets/img/ethan-thompson-zCYaSP5PHy8-unsplash.jpg')";
+    }
+    if (href === "#section-3") {
+      section1.style.backgroundImage =
+        "linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('/assets/img/gabriel-mccallin-bOGL5uq52X8-unsplash.jpg')";
+    }
+  }
+};
+
+navBar.addEventListener("mouseover", handleHoverImages);
+
+const allSection = document.querySelectorAll(".section");
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  // console.log(entry);
+  if (!entry.isIntersecting) return; //guard clause
+  entry.target.classList.remove("opacity-0");
+  observer.unobserve(entry.target); //as we scroll down page, unobserve webpage better for performance.
+};
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15, //15 visible. not right away
+});
+allSection.forEach(function (section) {
+  sectionObserver.observe(section);
+  // section.classList.add('section--hidden');
+});
+//sticky nav appears just before setion1, doesnt overlap any beginning part of section 1. + 90px will extend header section by 90x. use -90x. rem and percents dont work. nav appeared 90px before threshold reached. This set of values serves to grow or shrink each side of the root element's bounding box before computing intersections. dont hard code 90x, if dynamic  website different devices. will chaneg at certain pts. best to change it dynamically
