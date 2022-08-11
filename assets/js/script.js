4ebe8cd3b7fe1ba2fb5c757c7915ffa7210f6f13
Sinh Fu San <ss12932@my.bristol.ac.uk>
2022-08-11 15:01:42 +0100

    update portfolio with phase 2 projects

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,58 +1,58 @@
-"use strict";
+'use strict';
 
 //Declared Variables
-const introReveal = document.querySelectorAll(".intro-reveal");
-const header = document.querySelector(".header");
-const anchorLink = document.querySelector(".anchor-link");
-const learnMoreLink = document.querySelector(".learn-more-link");
-const hamburger = document.querySelector(".hamburger");
-const mobNavBar = document.querySelector(".mob-nav-bar");
-const section1 = document.querySelector("#section-1");
-const section2 = document.querySelector("#section-2");
-const section3 = document.querySelector("#section-3");
-const panels = document.querySelectorAll(".panel");
+const introReveal = document.querySelectorAll('.intro-reveal');
+const header = document.querySelector('.header');
+const anchorLink = document.querySelector('.anchor-link');
+const learnMoreLink = document.querySelector('.learn-more-link');
+const hamburger = document.querySelector('.hamburger');
+const mobNavBar = document.querySelector('.mob-nav-bar');
+const section1 = document.querySelector('#section-1');
+const section2 = document.querySelector('#section-2');
+const section3 = document.querySelector('#section-3');
+const panels = document.querySelectorAll('.panel');
 
 //Event Listeners
-window.addEventListener("load", function (event) {
+window.addEventListener('load', function (event) {
   event.preventDefault(); //on page load, unintended behaviour of section 1 title expanding to full size. this effect not intended.
   introReveal.forEach(function (span, i) {
     setTimeout(() => {
-      span.classList.remove("opacity-0");
-      span.classList.add("opacity-100");
-    }, 1200 * i);
+      span.classList.remove('opacity-0');
+      span.classList.add('opacity-100');
+    }, 900 * i);
   });
 });
 //smooth scrolling nav bar
-header.addEventListener("click", function (event) {
+header.addEventListener('click', function (event) {
   //needed or smooth scrolling wont work. will instantly go onto next section.
-  if (event.target.classList.contains("nav-link")) {
+  if (event.target.classList.contains('nav-link')) {
     event.preventDefault();
-    const linkSection = event.target.getAttribute("href");
-    document.querySelector(linkSection).scrollIntoView({ behavior: "smooth" });
+    const linkSection = event.target.getAttribute('href');
+    document.querySelector(linkSection).scrollIntoView({ behavior: 'smooth' });
   }
 });
 
 //smooth scrolling anchor link
-anchorLink.addEventListener("click", function (event) {
+anchorLink.addEventListener('click', function (event) {
   event.preventDefault();
-  section2.scrollIntoView({ behavior: "smooth" });
+  section2.scrollIntoView({ behavior: 'smooth' });
 });
-learnMoreLink.addEventListener("click", function (event) {
+learnMoreLink.addEventListener('click', function (event) {
   event.preventDefault();
-  section3.scrollIntoView({ behavior: "smooth" });
+  section3.scrollIntoView({ behavior: 'smooth' });
 });
 
 //hamburger menu toggle active class
-hamburger.addEventListener("click", function () {
-  hamburger.classList.toggle("active");
-  mobNavBar.classList.toggle("active");
+hamburger.addEventListener('click', function () {
+  hamburger.classList.toggle('active');
+  mobNavBar.classList.toggle('active');
 });
 
 //hamburger remove active class when link pressed.
-document.querySelectorAll(".nav-link").forEach((link) =>
-  link.addEventListener("click", function () {
-    hamburger.classList.remove("active");
-    mobNavBar.classList.remove("active");
+document.querySelectorAll('.nav-link').forEach((link) =>
+  link.addEventListener('click', function () {
+    hamburger.classList.remove('active');
+    mobNavBar.classList.remove('active');
   })
 );
 
@@ -60,15 +60,15 @@ const handleHoverImages = (e) => {
   const target = e.target;
 
   const hrefArr = [
-    "./assets/img/tom-w-zwdkxQZu0Ko-unsplash.jpg",
-    "./assets/img/ethan-thompson-zCYaSP5PHy8-unsplash.jpg",
-    "./assets/img/gabriel-mccallin-bOGL5uq52X8-unsplash.jpg",
+    './assets/img/tom-w-zwdkxQZu0Ko-unsplash.jpg',
+    './assets/img/ethan-thompson-zCYaSP5PHy8-unsplash.jpg',
+    './assets/img/gabriel-mccallin-bOGL5uq52X8-unsplash.jpg',
   ];
 
-  if (target.matches("a")) {
+  if (target.matches('a')) {
     // const hrefRandomIndex = Math.floor(Math.random() * hrefArr.length + 1);
     // section1.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${hrefArr[hrefRandomIndex]})`;
-    const hrefLink = target.getAttribute("href");
+    const hrefLink = target.getAttribute('href');
     hrefArr.forEach((href, i) => {
       if (hrefLink === `#section-${i + 1}`) {
         section1.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${href})`;
@@ -77,19 +77,19 @@ const handleHoverImages = (e) => {
   }
 };
 
-header.addEventListener("mouseover", handleHoverImages);
+header.addEventListener('mouseover', handleHoverImages);
 
-const allFadeSlideEl = document.querySelectorAll(".fadeSlideEl");
+const allFadeSlideEl = document.querySelectorAll('.fadeSlideEl');
 const revealFadeSlide = function (entries, observer) {
   const [entry] = entries;
   console.log(entry);
   // console.log(entry);
   if (!entry.isIntersecting) return; //guard clause
   const animArr = [
-    "translate-y-10",
-    "opacity-0",
-    "translate-x-10",
-    "-translate-x-10",
+    'translate-y-10',
+    'opacity-0',
+    'translate-x-10',
+    '-translate-x-10',
   ];
   animArr.forEach((el) => entry.target.classList.remove(el));
   observer.unobserve(entry.target); //as we scroll down page, unobserve webpage better for performance.
@@ -97,19 +97,19 @@ const revealFadeSlide = function (entries, observer) {
 const fadeSlideElObserver = new IntersectionObserver(revealFadeSlide, {
   root: null,
   threshold: 0.1, //10% visible. not right away
-  rootMargin: "-50px",
+  rootMargin: '-50px',
 });
 allFadeSlideEl.forEach(function (fadeSlideEl) {
   fadeSlideElObserver.observe(fadeSlideEl);
 });
 const removeActiveClasses = () => {
   panels.forEach((panel) => {
-    panel.classList.remove("active");
+    panel.classList.remove('active');
   });
 };
 panels.forEach((panel) => {
-  panel.addEventListener("click", () => {
+  panel.addEventListener('click', () => {
     removeActiveClasses();
-    panel.classList.add("active");
+    panel.classList.add('active');
   });
 });
